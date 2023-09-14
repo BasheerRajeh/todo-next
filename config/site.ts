18b5aa0a9669d9378cfbb366f5dfca50528fe5ff
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-09-14 08:45:20 +0300

    change icons, and fixing metadata

---
+++ config/site.ts
@@ -0,0 +1,37 @@
+import { IconDescriptor } from "next/dist/lib/metadata/types/metadata-types";
+
+type Site = {
+    url: string;
+    title: string;
+    name: string;
+    keywords: string[];
+    titleTemplate: string;
+    description: string;
+    favicons: IconDescriptor[];
+};
+
+export const site: Site = {
+    url:
+        process.env.NODE_ENV === "production"
+            ? "https://todo-nxt.vercel.app/"
+            : "http://localhost:3000",
+    title: "Todo",
+    name: "Basheer Rajeh",
+    keywords: ["Todo", "Basheer", "Rajeh"],
+    titleTemplate: "- Todo",
+    description: "The project that every web developer will create - Todo.",
+    favicons: [
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            url: "/favicon/favicon-16x16.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            url: "/favicon/favicon-32x32.png",
+        },
+    ],
+};
