639de4ed793181e2c52f6f348d5ee46e78074d1f
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-09-16 00:28:38 +0300

    fix clicking on delete to delete todo

--- components/todo/todoItem.tsx
+++ components/todo/todoItem.tsx
@@ -18,13 +18,13 @@ const TodoItem: React.FC<TodoProps> = ({ todo, onComplete, onDelete }) => {
         <div
             key={todo.id}
             className="flex items-center justify-between space-x-2 rounded-lg border px-4 py-2 hover:border-white transition-all"
-            onClick={() => onComplete(todo.id)}
         >
             <button
                 className={cn(
                     "w-6 h-6 rounded-lg bg-teal-100 p-1 transition-colors duration-300 hover:bg-teal-950",
                     { ["bg-teal-700"]: todo.completed }
                 )}
+                onClick={() => onComplete(todo.id)}
                 type="button"
             >
                 {todo.completed && <Check className="w-4 h-4" />}
@@ -33,6 +33,7 @@ const TodoItem: React.FC<TodoProps> = ({ todo, onComplete, onDelete }) => {
                 className={cn("flex-grow text-lg cursor-pointer", {
                     ["line-through"]: todo.completed,
                 })}
+                onClick={() => onComplete(todo.id)}
             >
                 {todo.title}
             </h2>
